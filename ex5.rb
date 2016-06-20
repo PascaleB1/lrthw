7d7119f23790aeb32168b7c9ab563c9df7c0ab46
PascaleB1 <barboteaupascale@yahoo.fr>
2016-06-20 16:30:53 +0200

    exercices ruby

---
+++ ex5.rb
@@ -0,0 +1,9 @@
+puts "Let's talk about #{'Zed A. Shaw'}."
+puts "He's #{74} inches tall."
+puts "He's #{180} pounds heavy."
+puts "Actually that's not too heavy."
+puts "He's got #{'Blue'} eyes and #{'Brown'} hair."
+puts "His teeth are usually #{'White'} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{35}, #{74}, and #{180} I get #{35 + 74 + 180}."
